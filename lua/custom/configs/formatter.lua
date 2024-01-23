619f80ab7735209b366bc7dcf32e55eb2a9bfc45
a.f.gareev <a.f.gareev@breezeventures.io>
2024-01-23 11:25:44 +0300

    fix

--- lua/custom/configs/formatter.lua
+++ lua/custom/configs/formatter.lua
@@ -1,28 +1,28 @@
 local M = {
-  filetype = {
-    json = {
-      require("formatter.filetypes.json").jq,
-    },
-    lua = {
-      require("formatter.filetypes.lua").stylua,
-    },
-    javascript = {
-      require("formatter.filetypes.javascript").prettier,
-    },
-    typescript = {
-      require("formatter.filetypes.typescript").prettier,
-    },
-    go = {
-      require("formatter.filetypes.go").gofmt,
-    },
-    --   ["*"] = {
-    --     require("formatter.filetypes.any").remove_trailing_whitespace,
-    --   },
-  },
+	filetype = {
+		json = {
+			require("formatter.filetypes.json").jq,
+		},
+		lua = {
+			require("formatter.filetypes.lua").stylua,
+		},
+		javascript = {
+			require("formatter.filetypes.javascript").prettier,
+		},
+		typescript = {
+			require("formatter.filetypes.typescript").prettier,
+		},
+		go = {
+			require("formatter.filetypes.go").gofmt,
+		},
+		["*"] = {
+			require("formatter.filetypes.any").remove_trailing_whitespace,
+		},
+	},
 }
 
 vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-  command = "FormatWriteLock",
+	command = "FormatWriteLock",
 })
 
 return M
